4fad37e6aee1c34d62f6dd174ac7c42310ba9128
Bruno Kilian <bkilian@twilio.com>
2021-11-17 13:32:09 +0100

    cleanup

--- src/FlexInsightsInsightsPerBpoPlugin.tsx
+++ src/FlexInsightsInsightsPerBpoPlugin.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import * as Flex from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
 
-import CustomTaskListContainer from './components/CustomTaskList/CustomTaskList.Container';
-import reducers, { namespace } from './states';
-
 const PLUGIN_NAME = 'FlexInsightsInsightsPerBpoPlugin';
 
 export default class FlexInsightsInsightsPerBpoPlugin extends FlexPlugin {
@@ -12,35 +9,5 @@ export default class FlexInsightsInsightsPerBpoPlugin extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof Flex }
-   * @param manager { Flex.Manager }
-   */
-  init(flex: typeof Flex, manager: Flex.Manager) {
-    this.registerReducers(manager);
-
-    const options: Flex.ContentFragmentProps = { sortOrder: -1 };
-    flex.AgentDesktopView
-      .Panel1
-      .Content
-      .add(<CustomTaskListContainer key="FlexInsightsInsightsPerBpoPlugin-component" />, options);
-  }
-
-  /**
-   * Registers the plugin reducers
-   *
-   * @param manager { Flex.Manager }
-   */
-  private registerReducers(manager: Flex.Manager) {
-    if (!manager.store.addReducer) {
-      // tslint: disable-next-line
-      console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${Flex.VERSION}`);
-      return;
-    }
-
-    manager.store.addReducer(namespace, reducers);
-  }
+  init(flex: typeof Flex, manager: Flex.Manager) {}
 }
